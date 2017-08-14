8f95d3b1869da8d5ff4132a7ea703ec8c3c6b3cb
kamejoin <jorge.martinez.villarroel@gmail.com>
2017-08-13 22:09:37 -0300

    2 to 1 complete write function

--- Python/write_functions.py
+++ Python/write_functions.py
@@ -1,32 +1,28 @@
+def write_kj(nodes, triangles, limites, **kwargs):
+	# kwargs handling
+	centered = False
+	file_clue = "no_name"
+	for name, value in kwargs.items():
+		if name == "name":
+			file_clue = value
+		elif name == "center" and value == True:
+			centered = True
 
-def write_kj(nodes, triangles):
+	# Create file
+	filename = "fail.kj"
+	if centered:
+		medios = []
+		for i in range(3):
+			medios += [(limites[i][1] - limites[i][0]) / 2. + limites[i][0]]
+		filename = "../../kj/" + file_clue + "_centered.kj"
+	else:
+		medios = [0, 0, 0]
+		filename = "../../kj/" + file_clue + ".kj"
 
-	outFile = open("../../kj/botadero.kj", 'w')
-	# Writing NODES
-	outFile.write("*** NODES ***\n" + str(len(nodes)) + "\n")
-	for i in range(len(nodes)):
-	    outFile.write(str(i) + ".")
-	    for j in range(3):
-	        outFile.write(" " + str(nodes[i][j]))
-	    outFile.write("\n")
-	# Writing TRIANGLES
-	outFile.write("*** TRIANGLES ***\n" + str(len(triangles)) + "\n")
-	for i in range(len(triangles)):
-	    outFile.write(str(i) + ".")
-	    for j in range(3):
-	        outFile.write(" " + str(triangles[i][j]))
-	    outFile.write("\n")
-	outFile.close()
+	print "Writing mesh info on", filename
+	outFile = open(filename, 'w')
 
 
-
-def write_centered_kj(nodes, triangles, limites):
-
-	medios = []
-	for i in range(3):
-		medios += [(limites[i][1] - limites[i][0]) / 2. + limites[i][0]]
-
-	outFile = open("../../kj/botadero_centered.kj", 'w')
 	# Writing NODES
 	outFile.write("*** NODES ***\n" + str(len(nodes)) + "\n")
 	for i in range(len(nodes)):
@@ -34,6 +30,7 @@ def write_centered_kj(nodes, triangles, limites):
 	    for j in range(3):
 	        outFile.write(" " + str(round(nodes[i][j] - medios[j], 5)))
 	    outFile.write("\n")
+	    
 	# Writing TRIANGLES
 	outFile.write("*** TRIANGLES ***\n" + str(len(triangles)) + "\n")
 	for i in range(len(triangles)):
